7f5adc4a919c94f2af5bf4edbd5bc83335646372
Patrick Lee <patrick@horologe.com.au>
2018-02-08 09:47:51 +1100

    Update theremin.js

--- theremin.js
+++ theremin.js
@@ -1,8 +1,10 @@
-c = new AudioContext();
-o = c.createOscillator();
-o.frequency.value = 261.63;
-g = c.createGain();
-g.gain.value = 0.05;
-o.start(0);
-o.connect(c.destination);
-Puck.write('LED1.set();\n');
+c = new AudioContext()
+o = c.createOscillator()
+o.frequency.value = 261.63
+g = c.createGain()
+g.gain.value = 0.05
+o.start(0)
+o.connect(c.destination)
+document.body.onclick=function(){
+  Puck.write('LED1.set();\n')
+}
