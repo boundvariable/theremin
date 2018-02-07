679c75dcd62fbb75056a756bdc522a912cee621f
Patrick Lee <patrick@horologe.com.au>
2018-02-08 09:39:05 +1100

    Update theremin.js

--- theremin.js
+++ theremin.js
@@ -2,6 +2,6 @@ c = new AudioContext();
 o = c.createOscillator();
 o.frequency.value = 261.63;
 g = c.createGain();
-g.gain.value = 0.1;
+g.gain.value = 0.05;
 o.start(0);
 o.connect(c.destination);
