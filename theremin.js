2fce2d105526238509ac978a5bf14b306f791f02
Patrick Lee <patrick@horologe.com.au>
2018-02-08 15:36:32 +1100

    Update theremin.js

--- theremin.js
+++ theremin.js
@@ -2,54 +2,57 @@ c=new AudioContext()
 o=c.createOscillator()
 o.frequency.value = 329.628
 g=c.createGain()
-xx=c.createWaveShaper();
-function mxx(amount) {
-  var k = typeof amount === 'number' ? amount : 50,
-    n_samples = 44100,
-    curve = new Float32Array(n_samples),
+w=c.createWaveShaper()
+function mx(k) {
+  var n_ = 44100,
+    curve = new Float32Array(n_),
     deg = Math.PI / 180,
     i = 0,
     x;
-  for ( ; i < n_samples; ++i ) {
-    x = i * 2 / n_samples - 1;
-    curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) );
+  for(; i < n_; ++i){
+    x = i * 2 / n_ - 1
+    curve[i]=(3+k)*x*20*deg/(Math.PI+k*Math.abs(x))
   }
   return curve;
 }
-xx.curve = mxx(400);
-xx.oversample = '4x';
+w.curve = mx(400);
+w.oversample = '4x';
 g.gain.value = 0
 g.connect(c.destination)
 o.start(0)
-o.connect(xx)
-xx.connect(g)
+o.connect(w)
+w.connect(g)
 c=0
 n="\n"
-function ln(_) {
-  var l = {};
+function w(_) {
+  var l={};
   try { 
-    l = JSON.parse(_)
-  } catch (e) {
+    l=JSON.parse(_)
+  } catch(e) {
     return
   }
-  console.log(l)
   if(!l.z && !l.v) return
   if(l.z) {
-    o.detune.value = Math.round(l.z/100)*300
+    o.detune.value=Math.round(l.z/100)*300
   }
-  if(l.v === 'on'){
-    g.gain.value = 0.1 
-  } else if(l.v === 'off'){
-    g.gain.value = 0
+  if(l.v==='on'){
+    g.gain.value=0.1 
+  }else if(l.v==='off'){
+    g.gain.value=0
   }
 }
-onclick=function(){  
-  if (c) {
+onclick=function(){
+  if(c){
+    document.body.style.background='red';
     c.write("Puck.magOff();\n")
-    c.close()
-    c = 0
+    c.write("reset();\n")
+    setTimeout(function() {
+      c.close()
+      c=0
+    }, 1500)
     return
   }
+  document.body.style.background='green';
   Puck.connect(function(k) {
     if(!k) return
     c=k
@@ -58,7 +61,7 @@ onclick=function(){
       b+=d
       i=b.indexOf(n)
       while(i>=0) {
-        ln(b.substr(0,i))
+        w(b.substr(0,i))
         b=b.substr(i+1)
         i=b.indexOf(n)
       }
