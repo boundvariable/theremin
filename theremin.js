7efb9070cdee6141b1f15b110d8fc6b8d9f4a7de
Patrick Lee <patrick@horologe.com.au>
2018-02-08 15:47:22 +1100

    Update theremin.js

--- theremin.js
+++ theremin.js
@@ -5,9 +5,9 @@ g=c.createGain()
 w=c.createWaveShaper()
 db=document.body
 function mx(k){
-var n_ = 44100,curve=new Float32Array(n_),deg=Math.PI/180,i = 0,x;
-for(; i < n_; ++i){
-x = i * 2 / n_ - 1
+var n_=44100,curve=new Float32Array(n_),deg=Math.PI/180,i=0,x;
+for(;i<n_;++i){
+x=i*2/n_-1
 curve[i]=(3+k)*x*20*deg/(Math.PI+k*Math.abs(x))
 }
 return curve
@@ -34,30 +34,30 @@ if(l.v==='on') g.gain.value=0.1
 if(l.v==='off') g.gain.value=0
 }
 onclick=function(){
-  if(c){
-    db.style.background='red'
-    c.write("Puck.magOff();\n")
-    c.close()
-    c=0
-    return
-  }
-  db.style.background='green'
-  Puck.connect(function(k){
-    if(!k) return
-    c=k
-    b=''
-    c.on("data",function(d){
-      b+=d
-      i=b.indexOf(n)
-      while(i>=0){
-        q(b.substr(0,i))
-        b=b.substr(i+1)
-        i=b.indexOf(n)
-      }
-    })
-    c.write("reset();\n")
-    setTimeout(function(){
-      c.write("Puck.magOn(20);Bluetooth.println(JSON.stringify(Puck.mag()));Puck.on('mag',function(m){Bluetooth.println(JSON.stringify(m));});setWatch(function(){Bluetooth.println(JSON.stringify({v:'on'}))},BTN,{edge:'rising',repeat:true});setWatch(function(){Bluetooth.println(JSON.stringify({v:'off'}))},BTN,{edge:'falling',repeat:true});NRF.on('disconnect',function(){reset()});\n")
-    }, 1500);
-  })
+if(c){
+db.style.background='red'
+c.write("Puck.magOff();\n")
+c.close()
+c=0
+return
+}
+db.style.background='green'
+Puck.connect(function(k){
+if(!k) return
+c=k
+b=''
+c.on("data",function(d){
+b+=d
+i=b.indexOf(n)
+while(i>=0){
+q(b.substr(0,i))
+b=b.substr(i+1)
+i=b.indexOf(n)
+}
+})
+c.write("reset();\n")
+setTimeout(function(){
+c.write("Puck.magOn(20);Bluetooth.println(JSON.stringify(Puck.mag()));Puck.on('mag',function(m){Bluetooth.println(JSON.stringify(m));});setWatch(function(){Bluetooth.println(JSON.stringify({v:'on'}))},BTN,{edge:'rising',repeat:true});setWatch(function(){Bluetooth.println(JSON.stringify({v:'off'}))},BTN,{edge:'falling',repeat:true});NRF.on('disconnect',function(){reset()});\n")
+}, 1500)
+})
 }
